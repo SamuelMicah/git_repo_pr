f830d4f4194672b1002d9ab8b7a332bfb129a287
Samuel Micah <151700080+SamuelMicah@users.noreply.github.com>
2023-12-27 10:30:19 +0000

    added user option to call git methods

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-class git:
+class Git:
     add = "adding"
     commit = "committing"
     push = "pushing"
@@ -15,3 +15,27 @@ class git:
 
     def run_push(self):
         print(self.push)
+
+git_instance = Git()
+
+while True:
+    user_input = input('''\nChoose one oof the following:
+1. add
+2. commit
+3. push
+4. merge
+5. exit
+: ''')
+    if(user_input == "1"):
+        Git.run_add(git_instance)
+    elif(user_input == "2"):
+        Git.run_commit(git_instance)
+    elif(user_input == "3"):
+        Git.run_push(git_instance)
+    elif(user_input == "4"):
+        Git.run_merge(git_instance)
+    elif(user_input == "5"):
+        break
+    else:
+        print("invalid option!")
+print("GoodBye!")
